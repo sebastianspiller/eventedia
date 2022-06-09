7d3b4ba502628ccbb712ca813cbbcbdbf38a10cf
Sebastian Spiller <spillersebastian@googlemail.com>
2022-06-09 19:49:17 +0200

    8th June 2022

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,31 @@ import React from 'react';
 import './App.css';
 import EventCard from './components/EventCard';
 
+
 function App() {
+  const eventarray = [{ort: "Düsseldorf"}, {ort: "Hamburg"}, {ort: "Berlin"}, {ort: "Köln"}]
+  console.log(eventarray)
+
+  const oneevent = eventarray.map((element)=>{
+    return <div><EventCard id={0} ort={element.ort} /></div>
+  })
+
   return (
-    <div>
-        <EventCard id="0" />
-        <EventCard id="0" />
-        <EventCard id="0" />
-        <EventCard id="0" />
+    <div className="card">
+      <div className='card-header'>
+        <div>Header</div>
+      </div>
+      <div className='card-body'>
+      {oneevent}
+        {/* <EventCard id={0} ort="Düsseldorf" />
+        <EventCard id={0} ort="Köln"/>
+        <EventCard id={0} ort="München"/>
+        <EventCard id={0} ort="Barcelona"/>
+        <EventCard id={0} ort="Barcelona"/> */}
+      </div>
+      <div className='card-footer'>
+          <div>Footer</div>
+      </div>
     </div>
   );
 }
--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import './EventCard.css'
 
-function EventCard({id}: {id: string}) {
+function EventCard({id, ort}: {id: number, ort:string}) {
   return (
     <div className='Card'>
 
-        <div>EventCard</div>
-        <img className="Bild" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/IIHS_crash_test_dummy_in_Hyundai_Tucson.jpg/220px-IIHS_crash_test_dummy_in_Hyundai_Tucson.jpg"/>
+        <div>EventTitel</div>
+        <img className="Bild" alt="Dies ist ein Dummy-Bild" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/IIHS_crash_test_dummy_in_Hyundai_Tucson.jpg/220px-IIHS_crash_test_dummy_in_Hyundai_Tucson.jpg"/>
+        <div className="Ort">Wo: {ort}</div>
+        <div className='Zeit'>
+          <div className='Beginn'>Beginn</div>
+          <div className='Ende'>Ende</div>
+        </div>
+
     </div>
   )
 }
 
 export default EventCard
+
+// 
